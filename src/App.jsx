8b797c0a65211a80c501e9d3c900fc327dd69277
Innders <49156310+Innders@users.noreply.github.com>
2024-06-24 14:21:54 +0100

    modal

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,29 @@
-import { Button } from "@ynput/ayon-react-components";
+// add to init
+import "@ynput/ayon-react-components/dist/style.css";
+import { Button, Dialog } from "@ynput/ayon-react-components";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  closeVersionsSelector,
+  openVersionsSelector,
+} from "./features/versionsSlice";
 
 function App() {
-  // use https://picsum.photos/seed to get a random image for each version
+  const dispatch = useDispatch();
+  const isOpen = useSelector((state) => state.versions.isOpen);
 
   return (
     <>
-      <Button>Open Versions</Button>
+      <Button onClick={() => dispatch(openVersionsSelector())}>
+        Open Versions
+      </Button>
+      <Dialog
+        isOpen={isOpen}
+        header="Versions"
+        onClose={() => dispatch(closeVersionsSelector())}
+        size="full"
+      >
+        <h3></h3>
+      </Dialog>
     </>
   );
 }
