d12013e2329fa1b398de494fd79a691eaeda36a3
Innders <49156310+Innders@users.noreply.github.com>
2024-06-24 15:25:28 +0100

    shortcuts

--- src/App.styled.js
+++ src/App.styled.js
@@ -1,3 +1,4 @@
+import { Dropdown } from "@ynput/ayon-react-components";
 import styled from "styled-components";
 
 export const Versions = styled.div`
@@ -8,9 +9,22 @@ export const Versions = styled.div`
   width: fit-content;
 `;
 
-export const VersionImg = styled.img``;
+export const VersionImg = styled.img`
+  height: 100%;
+  object-fit: contain;
+`;
 
 export const VersionsToolbar = styled.div`
   display: flex;
   gap: 4px;
+`;
+
+export const VersionDropdown = styled(Dropdown)`
+  .button {
+    background-color: var(--md-sys-color-surface-container-highest);
+
+    &:hover {
+      background-color: var(--md-sys-color-surface-container-highest-hover);
+    }
+  }
 `;
--- src/components/VersionsToolbar.jsx
+++ src/components/VersionsToolbar.jsx
@@ -1,32 +1,71 @@
 import { Button, Icon } from "@ynput/ayon-react-components";
 import * as Styled from "../App.styled";
+import { useCallback, useEffect } from "react";
 
 const VersionsToolbar = ({ versions = [], selected, onChange }) => {
-  const currentIndex = versions.findIndex((version) => version.id === selected);
-  const currentVersion = versions[currentIndex];
-  const previousVersion =
-    versions[currentIndex - 1] || currentVersion || "Unknown";
-  const nextVersion = versions[currentIndex + 1] || currentVersion || "Unknown";
-  const approvedVersion =
-    versions.find((version) => version.approved) || "None";
+  const sortedVersions = [...versions].sort((a, b) => a.version - b.version);
 
-  const handleVersionChange = (id) => {
-    onChange(id);
-  };
+  const options = sortedVersions.map((version) => ({
+    value: version.id,
+    label: version.version,
+  }));
+
+  const currentIndex = sortedVersions.findIndex(
+    (version) => version.id === selected
+  );
+  const currentVersion = sortedVersions[currentIndex];
+  const previousVersion =
+    sortedVersions[currentIndex - 1] || currentVersion || "Unknown";
+  const nextVersion =
+    sortedVersions[currentIndex + 1] || currentVersion || "Unknown";
+  const approvedVersion =
+    sortedVersions.find((version) => version.approved) || "None";
+
+  const handleVersionChange = useCallback(
+    (id) => {
+      onChange(id);
+    },
+    [onChange]
+  );
+
+  //   keyboard shortcuts
+  useEffect(() => {
+    const handleKeyDown = (e) => {
+      if (e.key === "ArrowLeft") {
+        handleVersionChange(previousVersion.id);
+      } else if (e.key === "ArrowRight") {
+        handleVersionChange(nextVersion.id);
+      } else if (e.key === "a") {
+        handleVersionChange(approvedVersion.id);
+      }
+    };
+
+    window.addEventListener("keydown", handleKeyDown);
+
+    return () => {
+      window.removeEventListener("keydown", handleKeyDown);
+    };
+  }, [previousVersion, nextVersion, approvedVersion, handleVersionChange]);
 
   return (
     <Styled.VersionsToolbar>
       <Button
         onClick={() => handleVersionChange(previousVersion.id)}
         icon="chevron_left"
+        style={{ paddingRight: 16 }}
+        disabled={!sortedVersions[currentIndex - 1]}
       >
         {previousVersion?.version}
       </Button>
-      <Button>{currentVersion?.version}</Button>
+      <Styled.VersionDropdown
+        value={[selected]}
+        options={options}
+        onChange={(v) => handleVersionChange(v[0])}
+      />
       <Button
         onClick={() => handleVersionChange(nextVersion.id)}
         style={{ paddingRight: 6 }}
-        selected
+        disabled={!sortedVersions[currentIndex + 1]}
       >
         {nextVersion?.version} <Icon icon="chevron_right" />
       </Button>
