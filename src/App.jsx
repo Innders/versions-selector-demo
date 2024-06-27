60dd2c18e138c6352f55cfc9c6ca5d7f07cf0825
Innders <49156310+Innders@users.noreply.github.com>
2024-06-27 07:49:43 +0100

    imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,8 @@
 import "@ynput/ayon-react-components/dist/style.css";
-import { Button } from "@ynput/ayon-react-components";
+import * as Styled from "./App.styled";
+import { Button, Dialog } from "@ynput/ayon-react-components";
+import { useDispatch, useSelector } from "react-redux";
+import versions from "./data.json";
 
 function App() {
   // use https://picsum.photos/seed to get a random image for each version
