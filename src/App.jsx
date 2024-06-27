06ddfd453b012a28108d6383c5094f8b6c45636e
Innders <49156310+Innders@users.noreply.github.com>
2024-06-27 07:49:17 +0100

    clean up imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 import "@ynput/ayon-react-components/dist/style.css";
+import * as Styled from "./App.styled";
 import { Button, Dialog } from "@ynput/ayon-react-components";
 import { useDispatch, useSelector } from "react-redux";
+import versions from "./data.json";
 import {
   closeVersionsSelector,
   openVersionsSelector,
   updateSelected,
 } from "./features/versionsSlice";
-import versions from "./data.json";
-import * as Styled from "./App.styled";
 import VersionImage from "./components/VersionImage";
 import VersionsToolbar from "./components/VersionsToolbar";
 
